7239a5645e5697089ae50f5cb2efbbb0d08f4789
Dave Copeland <david.copeland@livingsocial.com>
2019-07-30 13:36:11 -0400

    clean up favicons

--- config/copy_images.js
+++ config/copy_images.js
@@ -3,10 +3,12 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = [
   new CopyWebpackPlugin(
     [
-      { from: './site/images/**/*', to: './images', flatten: true, ignore: [ "Icon.pxm", "Icon.png" ] },
+      { from: './site/images/**/*', to: './images', flatten: true, ignore: [ "PR-flag.png", "site.webmanifest", "browserconfig.xml"  ] },
       { from: './site/pdf/**/*', to: './pdf', flatten: true },
       { from: './site/fonts/**/*', to: './fonts', flatten: true },
-      { from: './site/images/favicon.ico', to: '.', flatten: true }
+      { from: './site/images/favicon.ico', to: '.', flatten: true },
+      { from: './site/images/site.webmanifest', to: '.', flatten: true },
+      { from: './site/images/browserconfig.xml', to: '.', flatten: true }
     ],
     {
       debug: true
